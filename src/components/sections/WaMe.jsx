517fee4f2a79ade796f0f3b6828d6604ddd143fe
Insharah Developer <insharahattariya@gmail.com>
2026-04-28 21:21:22 +0500

    Hoo

--- src/components/sections/WaMe.jsx
+++ src/components/sections/WaMe.jsx
@@ -3,16 +3,16 @@ import wa_logo from "../../assets/whatsapp_logo.png";
 function WaMe() {
     return (
         <>
-            <div className="fixed bottom-3 left-3 z-[100] ">
+            <div className="fixed bottom-3 right-3 z-[100] ">
                 <a href="https://wa.me/+923000584969" target="_blank" className="flex items-center gap-2">
 
-                    {/* WhatsApp Icon */}
-                    <div className="w-15 p-1 rounded-2xl bg-[#eff6ff] shadow-lg cursor-pointer hover:scale-110 transition">
-                        <img src={wa_logo} alt="WhatsApp" />
-                    </div>
                     {/* Pop Message */}
                     <div className="bg-white text-green-600 px-3 py-1 rounded-xl shadow-lg text-sm font-medium animate-bounce">
                         Start Chatting
+                    </div>
+                    {/* WhatsApp Icon */}
+                    <div className="w-15 p-1 rounded-2xl bg-[#eff6ff] shadow-lg cursor-pointer hover:scale-110 transition">
+                        <img src={wa_logo} alt="WhatsApp" />
                     </div>
                 </a>
 
